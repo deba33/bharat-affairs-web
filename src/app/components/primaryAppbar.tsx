6316cc2991f6ffe99db37c3d3319456a24848974
deba33 <deba.dbs.bkt@gmail.com>
2024-05-15 00:32:52 +0530

    added link to download android app

--- src/app/components/primaryAppbar.tsx
+++ src/app/components/primaryAppbar.tsx
@@ -10,8 +10,17 @@ export default function PrimaryAppbar() {
       <p className="text-2xl m-4 max-md:mt-4 max-md:mb-0 text-slate-300">
         Bharat Affairs
       </p>
-      
-      
+      <div className="grow max-md:hidden"></div>
+      <div className="m-4">
+        <a
+          href="https://github.com/deba33/bharat-affairs-web/releases"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Download Android App
+        </a>
+      </div>
     </div>
   );
 }
